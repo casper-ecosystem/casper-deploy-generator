e5798f70f7dc427f6849066b7e6017004d0afacf
mateusz <mateusz@casperlabs.io>
2023-04-17 10:45:11 +0200

    Change loop to imperative

--- src/main.rs
+++ src/main.rs
@@ -20,17 +20,23 @@ fn main() {
 
     let limited_ledger_config = LimitedLedgerConfig::new(page_limit);
 
-    let data: Vec<ZondaxRepr> = undelegate_samples(&mut rng)
+    let mut id = 0;
+    let mut data: Vec<ZondaxRepr> = vec![];
+
+    for sample_deploy in undelegate_samples(&mut rng)
         .into_iter()
         .chain(delegate_samples(&mut rng))
         .chain(native_transfer_samples(&mut rng))
         .chain(redelegate_samples(&mut rng))
         .chain(generic_samples(&mut rng))
-        .enumerate()
-        .map(|(id, sample_deploy)| {
-            ledger::deploy_to_json(id, sample_deploy, &limited_ledger_config)
-        })
-        .collect();
+    {
+        data.push(ledger::deploy_to_json(
+            id,
+            sample_deploy,
+            &limited_ledger_config,
+        ));
+        id += 1;
+    }
 
     println!("{}", serde_json::to_string_pretty(&data).unwrap());
 }
